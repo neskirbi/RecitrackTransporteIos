081d5653d5df32db2a6ea8c8f1804c264d4c4f93
Jonathan Omar <johnnelemand@gmial.com>
2022-04-21 15:05:42 -0500

    mi primer comentario

---
+++ GPS/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  GPS
+//
+//  Created by Johnne Lemand on 12/04/22.
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions lauchOptions: [UIApplication.LauchOptionsKey: Any]?) -> Bool{
+        FirebaseApp.configure()
+        return true
+    }
+    
+    fun application (_
+}
